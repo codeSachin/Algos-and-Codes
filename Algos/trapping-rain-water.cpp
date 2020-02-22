a0d2bd8b4ca198cdbb577595406e4c1a39efd6c6
Sachin Saini <sachin17897@gmail.com>
2020-02-22 14:15:45 +0530

    trapping rain water solution from leet code

---
+++ Algos/trapping-rain-water.cpp
@@ -0,0 +1,47 @@
+//  https://leetcode.com/problems/trapping-rain-water/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int trap(vector<int> h) {
+        int sum = 0;
+        for(int i = 0; i < h.size();i++)
+        {
+            int leftMax = h[i];
+            int rightMax = h[i];
+            int j = i;
+            for(j=i;j>=0;j--)
+            {
+                //find left max from current index until you reach leftmost element
+                if(h[j] >= leftMax)
+                {
+                    leftMax = h[j];
+                }
+            }
+            int k = i;
+            for(k=i;k<h.size();k++)
+            {
+                //find right max here until you reach rightmost element
+                if(h[k] >= rightMax)
+                {
+                    rightMax = h[k];
+                }
+            }
+            //here we will have leftMax and Rightmax, so take min of them and that will be our overflowValue and add sum+= overflowValue - current cell's height;
+            //and we have to do this for each and every cell element of the array
+            int overflowValue = min(leftMax, rightMax);
+            sum += overflowValue - h[i];
+                
+        }
+        return sum;
+    }
+};
+
+int main()
+{
+    vector<int> v{0,1,0,2,1,0,3,2,1,2,1};
+    Solution *s = new Solution;
+    cout << s->trap(v);
+}
--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 |  Name  | Description |
 |  ----- | ----------- |
+| [trapping-rain-water.cpp](https://github.com/A7xSV/Algorithms-and-DS/blob/master/Codes/RunningMedian.cpp) | Trapping Rain Water Problem |
 | [RunningMedian.cpp](https://github.com/A7xSV/Algorithms-and-DS/blob/master/Codes/RunningMedian.cpp) | Update median for a continuous stream of numbers |
 | [TwinArrays.cpp](https://github.com/A7xSV/Algorithms-and-DS/blob/master/Codes/TwinArrays.cpp) | Minimum sum from two arrays such that elements have different indices |
